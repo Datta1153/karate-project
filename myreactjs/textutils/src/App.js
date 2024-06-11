37d2d4ded747eaa7fd514eaa5c49598d8a504749
Datta1153 <engineerdatta1153@gmail.com>
2024-06-11 11:22:15 +0530

    TextUtils

---
+++ myreactjs/textutils/src/App.js
@@ -0,0 +1,56 @@
+// import "./App.css";
+import Navbar from "./components/Navbar";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import TextForm from "./components/TextForm";
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  const [alert, setAlert] = useState(null);
+
+
+  const showAlert = (message, type)=>{
+
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 3000);
+    }
+
+  return (
+
+    <>
+    <Router>
+      <Navbar title="Textutils" aboutText="About" />
+      <Alert alert={alert}/>
+      <div className="container my-3">
+      
+        <Routes>
+          <Route exact path="/about" element={<About/>}>
+            
+          </Route>
+         <Route exact path="/" element={<TextForm showAlert={showAlert}heading="Enter the text to analyze below"/>   }>
+       
+        </Route>
+
+  </Routes>
+         </div>
+        </Router>
+       
+       
+    </>
+  );
+}
+
+export default App;
